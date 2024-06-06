7f71195e041683f2212c692d4ca949d920840ec9
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-06-06 17:58:51 -0400

    feat: add loading page to movie details page

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="spinner show" ></div>
+  )
+}
+
+export default Loading
--- client/src/components/MovieDetails.jsx
+++ client/src/components/MovieDetails.jsx
@@ -2,22 +2,29 @@ import React, { useState, usePar, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import BackToHomepageBtn from './BackToHomepageBtn'
 import API from '../api/axios.config'
+import Loading from './Loading';
 const MovieDetails = () => {
   const [movieDetailsData, setMovieDetailsData] = useState()
+  const [loading, setLoading] = useState(true); // Added loading state
   const {movieId} = useParams()
   const getMovieDetails = async () => {
     try {
       const response = await API.get(`movie/${movieId}`)
-      // console.log(response.data);
+      console.log(response.data);
       const data = await response.data
       setMovieDetailsData(data)
+      setLoading(false);
     } catch (error) {
       console.error(error)
+      setLoading(false);
     }
   }
   useEffect(() => {
     getMovieDetails()
   }, [])
+  if (loading) {
+    return <Loading />
+  }
   return (
     <section className="container">
       <BackToHomepageBtn />
@@ -33,7 +40,7 @@ const MovieDetails = () => {
     <div>
       <h2>Movie Title</h2>
       <p>
-        <i className="fas fa-star text-primary" />{movieDetailsData.vote_average} / 10
+        <i className="fas fa-star text-primary" />{movieDetailsData.vote_average} / 10 : ({movieDetailsData.vote_count} reviewers) 
       </p>
       <p className="text-muted">Release Date: XX/XX/XXXX</p>
       <p>
